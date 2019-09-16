d3cf7c3f1caf2be436d41aec6bcd605e0a366e30
Reljod Oreta <oretareljod@gmail.com>
2019-09-16 15:31:03 +0800

    Update in-out-detector.py
    Add lines that will able the code to read from json file

--- in_out_detector/in_out.py
+++ in_out_detector/in_out.py
@@ -1,39 +1,51 @@
 #!/bin/env python3
 
+# Native and web-based libraries
 import cv2
-import sys
+import sys;
+
+sys.path.append("")  # To be able to import from previous directory
 import numpy as np
-
-sys.path.append("")
-
+# My own python files.
+from Configuration import JSONCourtConfig
 from in_out_detector.helper import FieldParts
 from in_out_detector.helper import Visualize
 from in_out_detector.helper.TrackBar import TrackBar
 from in_out_detector.helper import Utils
 
-lower_hsv = (0, 34, 143)
-upper_hsv = (21, 176, 255)
-line_conf = (80, 165, 14)
-ANGLE_TOLERANCE = 5  # percentage (%)
-
-region_split_x = (17, 40, 60, 83)  # percentage (%)
-region_split_y = (22, 41, 65, 85)  # percentage (%)
+conf = "configuration/config.json"
 
 
 def main():
+    # This is the sample image
     IMAGE = "tenniscourt/orangecourt.jpg"
+
+    # Get configuration data for tennis court initial configuration
+    court = JSONCourtConfig(conf, "orange-court-1")
+
+    lower_hsv = court["lower-hsv"]
+    upper_hsv = court["upper-hsv"]
+    line_conf = court["line-conf"]
+    ANGLE_TOLERANCE = court["angle-tolerance"]
+    region_split_x = court["region-split-x"]
+    region_split_y = court["region-split-y"]
+
+    # Enter the configurations into the TrackBar
     tb = TrackBar(lower_hsv, upper_hsv, line_conf,
                   ANGLE_TOLERANCE, region_split_x, region_split_y)
-    tb.set_track_bar()
+    tb.set_track_bar()  # set TrackBar
 
+    # Read Image
     image = cv2.imread(IMAGE)
+    # Create black image with shape equal to the read image
     black = np.zeros(image.shape, dtype=np.uint8)
-
+    # Retain original image by copying
     image1 = image.copy()
 
     while True:
+        # Convert the RGB image into HSV
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
+        # Read and Get the data from the Track Bar configuration
         lower, higher, line_settings, angle_tolerance, split_x, split_y = tb.get_track_bar()
         angle_tolerance /= 10.0
 
@@ -42,21 +54,20 @@ def main():
         black_img, lines = Utils.get_boundaries(threshold, line_settings)
         line90, line0 = Utils.get_perpendicular_lines(lines, tolerance=angle_tolerance)
         points = Utils.get_intersection(line90, line0)
-        x_points, y_points = Utils.get_regions(image, split_x, split_y)
         points_x, points_y = Utils.get_corner_points(black_img, points, split_x, split_y)
+        # x_points, y_points = Utils.get_regions(image, split_x, split_y)
 
         boundary_doubles = FieldParts.get_boundary_doubles(points_x, points_y)
         boundary_singles = FieldParts.get_boundary_singles(points_x, points_y)
-
         boundary_doubles_left = FieldParts.get_boundary_doubles_left(points_x, points_y)
         boundary_doubles_right = FieldParts.get_boundary_doubles_right(points_x, points_y)
-
         boundary_singles_left = FieldParts.get_boundary_singles_left(points_x, points_y)
         boundary_singles_right = FieldParts.get_boundary_singles_right(points_x, points_y)
 
         # line_black = Visualize.draw_lines(black_img, line90, line0)
         # region_image = Visualize.draw_regions(image, x_points, y_points)
 
+        # Visualize the Boundaries:
         final_image = Visualize.draw_boundary(image1, black, boundary_doubles_left,
                                               color=(120, 120, 0), thickness=2,
                                               fade=0.4, boundary=False, area=True)
@@ -80,7 +91,7 @@ def main():
         cv2.imshow("Threshold Image", threshold)
         cv2.imshow("Lined Image", black_img)
         # cv2.imshow("Line Black Image", line_black)
-        # cv2.imshow("Region Image", region_image)
+        # # cv2.imshow("Region Image", region_image)
         cv2.imshow("Final Image", final_image)
 
         k = cv2.waitKey(10) & 0xFF
